c9dbcd3911780d11a196862f1f398d6e394cdebc
k2projct <kopczkrzy@gmail.com>
2020-08-27 21:09:00 +0100

    imgs optimised

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from './components/Footer';
 
 function App() {
     return (
-        <div>
+        <div className='app'>
             <Home />
             <Footer />
         </div>
