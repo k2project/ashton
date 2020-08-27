db75eb20aee10acc8d998e3deaefa8ef885d2a1c
k2projct <kopczkrzy@gmail.com>
2020-08-27 20:34:20 +0100

    Updates

---
+++ precache-manifest.c1f4e6a64ecd4d33b150da7a446f805d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42fe821401f131623f8296a94cbc0349",
+    "url": "/ashton/index.html"
+  },
+  {
+    "revision": "b994c7af4148b9266343",
+    "url": "/ashton/static/css/main.d0835029.chunk.css"
+  },
+  {
+    "revision": "314f502c2b1bd1d5062e",
+    "url": "/ashton/static/js/2.0ba280d2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ashton/static/js/2.0ba280d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b994c7af4148b9266343",
+    "url": "/ashton/static/js/main.99965191.chunk.js"
+  },
+  {
+    "revision": "ae3a149921d8bf27f665",
+    "url": "/ashton/static/js/runtime-main.746bd8de.js"
+  },
+  {
+    "revision": "d926b1a658036700e4ebbfd864cc2aa8",
+    "url": "/ashton/static/media/SchaufussStarLogo.d926b1a6.png"
+  },
+  {
+    "revision": "a1a9e84e61ff7859ac68b7631796d595",
+    "url": "/ashton/static/media/ashton2.a1a9e84e.jpg"
+  },
+  {
+    "revision": "472914f9db5f48cbeef1c55ac0f637a1",
+    "url": "/ashton/static/media/efb.472914f9.jpg"
+  },
+  {
+    "revision": "7665576feb603f46686e51ee231aa5db",
+    "url": "/ashton/static/media/rte.7665576f.jpg"
+  },
+  {
+    "revision": "e34cd84b7891c834ff978fde39622db5",
+    "url": "/ashton/static/media/sse.e34cd84b.jpg"
+  }
+]);
